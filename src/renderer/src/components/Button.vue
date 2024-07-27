e7dae2339f6c4ba7e497eb2f9e9901fd76000bcb
unknown <eureka77035@gmail.com>
2024-07-27 16:47:29 +0800

    feat:添加分辨率组件

---
+++ src/renderer/src/components/Button.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex justify-center">
+    <el-button color="#0a65cc" size="small">停止转码</el-button>
+    <el-button color="#ff3838" size="small">停止转码</el-button>
+  </main>
+</template>
---
+++ src/renderer/src/components/Frame.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="bg-white p-3">
+    <section class="flex gap-4">
+      <div class="flex-auto">
+        <p>分辨率</p>
+        <el-select>
+          <el-option></el-option>
+        </el-select>
+      </div>
+      <div class="flex-auto">
+        <p>帧</p>
+        <el-select>
+          <el-option></el-option>
+        </el-select>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+p {
+  @apply mb-2 text-xs text-gray-500
+}
+</style>
