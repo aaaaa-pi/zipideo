08a0a4e1164bebd6798a33bf390269bd52f5298d
unknown <eureka77035@gmail.com>
2024-07-27 16:47:04 +0800

    feat:添加转换组件

---
+++ src/renderer/src/components/Actions.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { InboxOut, UpdateRotation } from '@icon-park/vue-next'
+</script>
+
+<template>
+  <main>
+    <section class="flex justify-center items-center gap-4">
+      <div class="button ml-2">
+        <inbox-out theme="filled" size="36" fill="#0a65cc" />
+      </div>
+      <div class="button mr-2">
+        <update-rotation theme="outline" size="36" fill="#0a65cc" />
+      </div>
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  @apply h-20 rounded-lg bg-white flex justify-center items-center text-slate-600 flex-auto;
+}
+</style>
