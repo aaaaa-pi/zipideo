dde8d103f7d41445460c9a14fd86665b83742edc
unknown <eureka77035@gmail.com>
2024-07-27 16:48:07 +0800

    feat: 添加导航栏和视频列表组件

---
+++ src/renderer/src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="text-slate-600 border-b bg-gray-50 flex justify-between px-3 py-2 drag">
+    <h5 class="opacity-80" style="font-family: 'Press Start 2P', cursive">zipideo</h5>
+    <section class="flex justify-between gap-2 multifont text-xs items-center">
+      <router-link :to="{ name: 'home' }">转码</router-link>
+      <router-link :to="{ name: 'setting' }">配置</router-link>
+    </section>
+  </main>
+</template>
---
+++ src/renderer/src/components/VideoList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main">
+    <p>视频列表</p>
+    <section class="flex flex-auto">
+      <span class="m-auto text-gray-500">暂无数据</span>
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.main {
+  @apply bg-white overflow-y-auto h-80 rounded-lg mx-2 mb-2 flex flex-col border-dashed border-2 border-blue-600;
+}
+p {
+  @apply m-2 text-xs text-gray-500;
+}
+</style>
