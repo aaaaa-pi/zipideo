971eee4dd85a045bf828e6085c45097cfeee062c
unknown <eureka77035@gmail.com>
2024-07-28 23:12:51 +0800

    feat: 添加分辨率和帧数的配置

--- src/renderer/src/components/Frame.vue
+++ src/renderer/src/components/Frame.vue
@@ -1,18 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useConfigStore } from '@renderer/stores/useConfigStore'
+const { config } = useConfigStore()
+</script>
 
 <template>
   <main class="bg-white p-3">
     <section class="flex gap-4">
       <div class="flex-auto">
         <p>分辨率</p>
-        <el-select>
-          <el-option></el-option>
+        <el-select v-model="config.size" placeholder="请选择转换的分辨率">
+          <el-option
+            v-for="(item, index) in config.sizes"
+            :key="index"
+            :label="item"
+            :value="item"
+          ></el-option>
         </el-select>
       </div>
       <div class="flex-auto">
         <p>帧数</p>
-        <el-select>
-          <el-option></el-option>
+        <el-select v-model="config.frame" placeholder="请选择转换的帧数">
+          <el-option
+            v-for="(item, index) in config.frames"
+            :key="index"
+            :label="item"
+            :value="item"
+          ></el-option>
         </el-select>
       </div>
     </section>
@@ -21,6 +34,6 @@
 
 <style lang="scss" scoped>
 p {
-  @apply mb-2 text-xs text-gray-500
+  @apply mb-2 text-xs text-gray-500;
 }
 </style>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,13 +2,16 @@ import '@renderer/assets/tailwind.css'
 import '@renderer/assets/main.scss'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from '@renderer/router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(ElementPlus)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
