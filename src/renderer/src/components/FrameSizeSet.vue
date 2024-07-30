013b7e168eb204fefc967d84fe31861854772299
unknown <eureka77035@gmail.com>
2024-07-31 00:07:10 +0800

    feat:封装分辨率和帧数设置组件

---
+++ src/renderer/src/components/FrameSizeSet.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { DataType } from '@renderer/types'
+interface Props {
+  type: DataType
+  placeholder?: string
+  tip?: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <main>
+    <el-select :placeholder="props.placeholder">
+      <el-option></el-option>
+    </el-select>
+    <div class="flex gap-1 mt-2 items-center">
+      <el-input :placeholder="props.tip" size="default" clearable></el-input>
+      <el-button color="#0a65cc" size="default">增加</el-button>
+    </div>
+  </main>
+</template>
--- src/renderer/src/view/Setting.vue
+++ src/renderer/src/view/Setting.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import Card from '@renderer/components/Card.vue'
+import FrameSizeSet from '@renderer/components/FrameSizeSet.vue'
 </script>
 
 <template>
   <main>
-    <Card title="分辨率"></Card>
-    <Card title="帧数"></Card>
+    <Card title="分辨率">
+      <FrameSizeSet type="size" placeholder="设置的分辨率" tip="请输入'1024x720'格式的值" />
+    </Card>
+    <Card title="帧数">
+      <FrameSizeSet type="frame" placeholder="设置的帧数" tip="请输入'60'格式的值" />
+    </Card>
   </main>
 </template>
