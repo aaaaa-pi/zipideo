a5db38b3c148284f3fa3f0f89e3d0150d4b6948b
unknown <eureka77035@gmail.com>
2024-08-02 00:21:28 +0800

    feat: 类定义Ffmpeg逻辑代码

---
+++ src/main/ffmpeg.ts
@@ -0,0 +1,37 @@
+import ffmpegPath from '@ffmpeg-installer/ffmpeg'
+import ffprobePath from '@ffprobe-installer/ffprobe'
+import ffmpeg from 'fluent-ffmpeg'
+ffmpeg.setFfmpegPath(ffmpegPath.path)
+ffmpeg.setFfprobePath(ffprobePath.path)
+
+export type CompressOptions = {
+  file: string
+  fps: number
+  size: string
+}
+
+export class Ffmpeg {
+  ffmpeg: ffmpeg.FfmpegCommand
+  constructor(private options: CompressOptions) {
+    this.ffmpeg = ffmpeg(this.options.file)
+  }
+  progressEvent(progress) {
+    console.log('Processing: ' + progress.percent + '% done')
+  }
+  error(err) {
+    console.log('An error occurred: ' + err.message)
+  }
+  end() {
+    console.log('Processing finished !')
+  }
+  run() {
+    this.ffmpeg
+      .videoCodec('libx264')
+      .size(this.options.size) // 设置分辨率
+      .fps(this.options.fps) // 设置帧数
+      .on('progress', this.progressEvent.bind(this))
+      .on('error', this.error.bind(this))
+      .on('end', this.end.bind(this))
+      .save('/path/to/file_finish.avi') // 保存的文件路径
+  }
+}
