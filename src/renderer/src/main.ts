b509965e1f50eb6b5545aecffbe376d67faa4434
unknown <eureka77035@gmail.com>
2024-08-03 00:54:08 +0800

    feat: 添加pinia插件进行数据持久化配置

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -5,11 +5,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from '@renderer/router'
 
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 app.use(ElementPlus)
 app.use(router)
--- src/renderer/src/stores/useConfigStore.ts
+++ src/renderer/src/stores/useConfigStore.ts
@@ -2,24 +2,30 @@ import { VideoType } from '@renderer/types'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useConfigStore = defineStore('config', () => {
-  const config = ref({
-    sizes: ['1920x1080', '1024x720'],
-    size: '1920x1080',
-    frames: ['60', '30'],
-    frame: '60',
-    files: [] as VideoType[],
-    saveFilePath: ''
-  })
-  const setSaveFilePath = (path: string) => {
-    config.value.saveFilePath = path
-  }
+export const useConfigStore = defineStore(
+  'config',
+  () => {
+    const config = ref({
+      sizes: ['1920x1080', '1024x720'],
+      size: '1920x1080',
+      frames: ['60', '30'],
+      frame: '60',
+      files: [] as VideoType[],
+      saveFilePath: ''
+    })
+    const setSaveFilePath = (path: string) => {
+      config.value.saveFilePath = path
+    }
 
-  const fetchDefaultSavePath = async () => {
-    const path = await window.api.getDefaultSavePath()
-    config.value.saveFilePath = path
-    console.log(config.value.saveFilePath);
-  }
+    const fetchDefaultSavePath = async () => {
+      const path = await window.api.getDefaultSavePath()
+      config.value.saveFilePath = path
+      console.log(config.value.saveFilePath)
+    }
 
-  return { config, fetchDefaultSavePath, setSaveFilePath }
-})
+    return { config, fetchDefaultSavePath, setSaveFilePath }
+  },
+  {
+    persist: true
+  }
+)
