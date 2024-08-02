80386f08e3ffc848fcc1a84c6f1c239de71dc3ee
unknown <eureka77035@gmail.com>
2024-08-02 23:35:32 +0800

    fix: 修复设置按钮不能点击的问题

--- src/renderer/src/components/Navbar.vue
+++ src/renderer/src/components/Navbar.vue
@@ -4,8 +4,8 @@
   <main class="text-slate-600 border-b bg-gray-50 flex justify-between px-3 py-2 drag">
     <h5 class="opacity-80" style="font-family: 'Press Start 2P', cursive">zipideo</h5>
     <section class="flex justify-between gap-2 multifont text-xs items-center">
-      <router-link :to="{ name: 'home' }">转码</router-link>
-      <router-link :to="{ name: 'setting' }">配置</router-link>
+      <router-link :to="{ name: 'home' }" class="nodrag">转码</router-link>
+      <router-link :to="{ name: 'setting' }" class="nodrag">配置</router-link>
     </section>
   </main>
 </template>
